05d5a63b0d6bab1355a25ee6ae22af23ca82880c
Matthieu Giraud <matthieu.giraud99@gmail.com>
2021-10-02 15:08:54 +0200

    Trigger enter ok

--- ludum-dare-49/Assets/Scripts/Puzzle/Trigger.cs
+++ ludum-dare-49/Assets/Scripts/Puzzle/Trigger.cs
@@ -22,8 +22,32 @@ namespace Puzzle
 
         public void OnCollisionEnter(Collision collision)
         {
-            if (_mode == Mode.COLLIDE && collision.gameObject.tag == "Ball")
+            if (CollisionIsValid(collision.gameObject))
                 triggered?.Invoke(this, null);
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (CollisionIsValid(other.gameObject))
+            {
+                Debug.Log(other);
+                triggered?.Invoke(this, null);
+            }
+        }
+
+        public bool CollisionIsValid(GameObject go)
+        {
+            return _mode == Mode.COLLIDE && IsBall(go) && LayerIsValid(go);
+        }
+
+        public bool IsBall(GameObject go)
+        {
+            return go.tag == "Ball" || (go.transform.parent && go.transform.parent.tag == "Ball");
+        }
+
+        public bool LayerIsValid(GameObject go)
+        {
+            return go.layer != LayerMask.NameToLayer("IgnoreTrigger");
         }
     }
 }
