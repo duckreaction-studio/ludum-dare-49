e84957acd2b6289b8ec6f19136469621c25dbbc8
Matthieu Giraud <matthieu.giraud99@gmail.com>
2021-10-02 22:11:58 +0200

    Refactoring

---
+++ ludum-dare-49/Assets/Scripts/Ball/State.cs
@@ -0,0 +1,43 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ball
+{
+    public class State : MonoBehaviour
+    {
+        private Vector3 _startPosition;
+        private Quaternion _startRotation;
+
+        Rigidbody _rigidbody;
+        public new Rigidbody rigidbody
+        {
+            get
+            {
+                if (_rigidbody == null)
+                {
+                    _rigidbody = GetComponent<Rigidbody>();
+                }
+                return _rigidbody;
+            }
+        }
+
+        void Awake()
+        {
+            _startPosition = transform.position;
+            _startRotation = transform.rotation;
+        }
+
+        [ContextMenu("Reset to start")]
+        [Button(ButtonSizes.Medium)]
+
+        public void ResetToStart()
+        {
+            rigidbody.isKinematic = true;
+            transform.position = _startPosition;
+            transform.rotation = _startRotation;
+            rigidbody.isKinematic = false;
+        }
+    }
+}
