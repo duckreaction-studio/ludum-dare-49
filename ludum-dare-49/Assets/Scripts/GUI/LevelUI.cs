f9835982c89feba777aabb324f2a16dea02a0775
Matthieu Giraud <matthieu.giraud99@gmail.com>
2021-10-04 10:59:32 +0200

    Transition level presque ok

--- ludum-dare-49/Assets/Scripts/GUI/LevelUI.cs
+++ ludum-dare-49/Assets/Scripts/GUI/LevelUI.cs
@@ -2,7 +2,9 @@ using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace GUI
@@ -52,10 +54,60 @@ namespace GUI
         {
             if (_levelState.currentState == LevelState.State.Replay)
             {
-                string[] unload = { "Scenes/UI/LevelUI", "Scenes/Tests/TestLevel01" };
-                string[] load = { "Scenes/Tests/TestLevel01", "Scenes/UI/LevelUI" };
-                _sceneService.StartSceneTransition(unload, load);
+                string[] unload = { "Scenes/UI/LevelUI", GetCurrentLevelName() };
+                List<string> load = new List<string>();
+                var nextLevel = GetNextLevelName();
+                if (SceneExist(nextLevel))
+                {
+                    load.Add(nextLevel);
+                    load.Add("Scenes/UI/LevelUI");
+                }
+                else
+                {
+                    load.Add("Scenes/MainMenu");
+                }
+                _sceneService.StartSceneTransition(unload, load.ToArray());
             }
+        }
+
+        public bool SceneExist(string name)
+        {
+            for (var i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+            {
+                if (SceneUtility.GetScenePathByBuildIndex(i) == name)
+                    return true;
+            }
+            return false;
+        }
+
+        public string GetNextLevelName()
+        {
+            int number = GetCurrentLevelNumber();
+            if (number < 0)
+                return "";
+            number++;
+            return string.Format("Scenes/Levels/Level{0:D2}", number);
+        }
+
+        public int GetCurrentLevelNumber()
+        {
+            var currentScene = GetCurrentLevelName();
+            var sceneNumber = Regex.Match(currentScene, @"\d+").Value;
+            if (!string.IsNullOrEmpty(sceneNumber))
+                return int.Parse(sceneNumber);
+            return -1;
+        }
+
+        public string GetCurrentLevelName()
+        {
+            int countLoaded = SceneManager.sceneCount;
+            for (int i = 0; i < countLoaded; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                if (scene.path.Contains("Scenes/Levels/Level"))
+                    return scene.path.Replace("Assets/", "");
+            }
+            return "";
         }
 
         public void Quit()
